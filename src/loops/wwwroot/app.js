4ad39517b2836b26a6273b2dbcd442cf1fdf765c
John Thompson <johnny918819@gmail.com>
2017-01-23 12:25:32 -0800

    learing about loops

---
+++ src/loops/wwwroot/app.js
@@ -0,0 +1,10 @@
+for (var i = 0; i < 11; i += 2) {
+    console.log("the loop is currently at itteration " + i);
+}
+var colors = [
+    'red', 'blue', 'green'
+];
+//Log each value in the colors array
+for (i = 0; i < colors.length; i++) {
+    console.log("The loop is at itteration " + i + ", and the value of colors[] at that position is " + colors[i]);
+}
---
+++ src/loops/wwwroot/app.ts
@@ -0,0 +1,13 @@
+﻿for (let i = 0; i < 11; i+=2) {
+    console.log(`the loop is currently at itteration ${i}`);
+}
+
+let colors: string[] = [
+    'red', 'blue', 'green'
+];
+
+//Log each value in the colors array
+
+for (i = 0; i < colors.length; i++) {
+    console.log(`The loop is at itteration ${i}, and the value of colors[] at that position is ${colors[i]}`);
+}
